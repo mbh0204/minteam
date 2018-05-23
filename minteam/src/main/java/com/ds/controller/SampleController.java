292c3d5cd5a834cb639ac96bd1f2b4562177a173
ds <ds@ds606-main>
2018-05-23 11:40:52 +0900

    /sample/test1 요청 경로 추가

--- minteam/src/main/java/com/ds/controller/SampleController.java
+++ minteam/src/main/java/com/ds/controller/SampleController.java
@@ -2,11 +2,13 @@ package com.ds.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/sample")
 public class SampleController {
 	
+	@RequestMapping(value = "/test1", method = RequestMethod.GET)
 	public void test1() throws Exception {
 		
 	}
