e639a2946007a2e591bbb2a2dada1ecf8f34b383
ds <ds@ds606-02>
2018-05-23 12:00:59 +0900

    부산 메뉴 작업 시작

---
+++ minteam/src/main/java/com/ds/controller/MenuController.java
@@ -0,0 +1,15 @@
+package com.ds.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+
+	@RequestMapping(value = "/busan", method = RequestMethod.GET)
+	public String busan() throws Exception{
+		return "/menu/busanMenu";
+	}
+}
